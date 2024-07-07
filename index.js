41c17f73289955ce364afa9666741e95954a2b48
SreejaSanisetty74 <sreejasanisetty@gmail.com>
2024-07-07 23:58:41 +0530

    Add files via upload
    Counter(ReactRouting)

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import MyForm from './MyForm';
+import Counter from './Counter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyForm/>
+    <Counter/>
   </React.StrictMode>
 );
 
